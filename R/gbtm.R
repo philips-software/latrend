fbe5a9285d067a791491e6735173c46adb570d0f
Niek Den Teuling <none@none>
2018-06-13 13:19:45 +0200

    Implemented GBTM

---
+++ R/gbtm.R
@@ -0,0 +1,29 @@
+#' @export
+#' @import lcmm
+#' @title Group-based trajectory modeling
+#' @seealso cluslong_gmm
+#' @inheritParams cluslong
+#' @inheritParams cluslong_gmm
+#' @param start Model initialization method. Either gridsearch or kml
+#' @param startMaxIter Number of model optimization iterations for the initialization method
+cluslong_gbtm = function(data,
+                        numClus=2:3,
+                        maxIter=NULL,
+                        numRuns=25,
+                        fixed,
+                        mixture,
+                        start=c('gridsearch', 'kml'),
+                        startMaxIter=ifelse(start[1] == 'gridsearch', 20, 0),
+                        idCol,
+                        timeCol,
+                        valueCol,
+                        resultFun=function(clr, cluslongResult) cluslongResult,
+                        keep=getOption('cluslong.keep', 'all'),
+                        verbose=TRUE,
+                        seed=NULL) {
+    do.call(cluster_longitudinal, c(mget(names(formals()), sys.frame(sys.nframe())),
+                                    clusterFun=cl_gmm,
+                                    random=~-1,
+                                    diagCov=TRUE,
+                                    classCov=FALSE))
+}
