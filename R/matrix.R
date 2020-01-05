fac377245b3d9021f0e74b1eb838aacbd828c0a6
Niek Den Teuling <none@none>
2020-01-05 15:18:12 +0100

    Additional checks for rowColumns. Added tests.

--- R/matrix.R
+++ R/matrix.R
@@ -1,7 +1,7 @@
 #' @title Select the specified column per row
 #' @keywords internal
 rowColumns = function(x, i) {
-  assert_that(is.matrix(x))
-  assert_that(length(i) == nrow(x))
+  assert_that(is.matrix(x) && all(is.finite(i)))
+  assert_that(length(i) == nrow(x) && max(i) <= nrow(x) && min(i) >= 1)
   x[cbind(1:nrow(x), i)]
 }
