6d9bf3f20e332a013f23e39bf53da6489aca39e8
Niek Den Teuling <none@none>
2020-01-25 23:58:59 +0100

    meltRepeatedMeasures() and dcastRepeatedMeasures()

--- R/matrix.R
+++ R/matrix.R
@@ -5,3 +5,63 @@ rowColumns = function(x, i) {
   assert_that(length(i) == nrow(x) && max(i) <= nrow(x) && min(i) >= 1)
   x[cbind(1:nrow(x), i)]
 }
+
+#' @export
+#' @title Convert a repeated measures data matrix to a data.frame
+#' @param data The `matrix` containing a trajectory on each row.
+#' @param id The id column name.
+#' @param time The time column name.
+#' @param response The response column name.
+#' @param ids A `vector` specifying the id names. Should match the number of rows of `data`.
+#' @param times A `vector` specifying the times of the measurements. Should match the number of columns of `data`.
+#' @param as.data.table Whether to return the result as a `data.table`, or a `data.frame` otherwise.
+#' @return A `data.table` or `data.frame` containing the repeated measures.
+meltRepeatedMeasures = function(data,
+                                  id=getOption('cluslong.id'),
+                                  time=getOption('cluslong.time'),
+                                  response=getOption('cluslong.response'),
+                                  ids=rownames(data),
+                                  times=as.numeric(colnames(data)),
+                                  as.data.table=FALSE) {
+  assert_that(is.matrix(data))
+  assert_that(is.character(id), is.character(time), is.character(response))
+
+  ids = rownames(data)
+  times = colnames(data)
+
+  if(length(ids) == 0) {
+    ids = seq_len(nrow(data))
+  }
+  if(length(times) == 0) {
+    times = seq(0, 1, length.out=ncol(data))
+  }
+
+  assert_that(length(ids) == nrow(data), length(times) == ncol(data))
+
+  dt = data.table(Id=rep(ids, each=ncol(data)),
+             Time=rep(times, nrow(data)),
+             Response=as.numeric(t(data)))
+  setnames(dt, c('Id', 'Time', 'Response'), c(id, time, response))
+
+  if(as.data.table) {
+    dt
+  } else {
+    as.data.frame(dt)
+  }
+}
+
+#' @export
+#' @title Cast a longitudinal data.frame to a matrix
+#' @inheritParams meltRepeatedMeasures
+#' @return A `matrix` of the repeated measures.
+dcastRepeatedMeasures = function(data,
+                                 id=getOption('cluslong.id'),
+                                 time=getOption('cluslong.time'),
+                                 response=getOption('cluslong.response')) {
+  assert_that(has_name(data, c(id, time, response)))
+  df = reshape2::dcast(data, formula=as.formula(paste(id, time, sep='~')), value.var=response, fill=NA*0, fun.aggregate=mean)
+  dataMat = as.matrix(df[, -1])
+  rownames(dataMat) = df[[1]]
+  colnames(dataMat) = sort(unique(data[[time]]))
+  return(dataMat)
+}
--- tests/testthat/test_matrix.R
+++ tests/testthat/test_matrix.R
@@ -10,3 +10,23 @@ test_that('rowColumns', {
   expect_error(rowColumns(m, c(1,4,1,1)))
   expect_error(rowColumns(m, c(0,1,1,1)))
 })
+
+
+test_that('meltRepeatedMeasures', {
+  m = matrix(1:12, nrow=3)
+  meltRepeatedMeasures(m) %>%
+    expect_is('data.frame') %>%
+    expect_named(c('Id', 'Time', 'Value')) %T>%
+    {expect_equal(nrow(.), length(m))} %T>%
+    {expect_equal(.$Value, as.numeric(t(m)))}
+})
+
+test_that('dcastRepeatedMeasures', {
+  m = matrix(1:12, nrow=3)
+  df = meltRepeatedMeasures(m)
+  dcastRepeatedMeasures(df) %>%
+    expect_is('matrix') %T>%
+    {expect_equal(nrow(.), nrow(m))} %T>%
+    {expect_equal(ncol(.), ncol(m))} %T>%
+    {expect_equal(as.numeric(.), as.numeric(m))}
+})
