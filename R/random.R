3b7d0b9345bbcf554854e17a14ea83737e595fb3
Niek Den Teuling <none@none>
2020-05-19 23:46:24 +0200

    localRNG

---
+++ R/random.R
@@ -0,0 +1,11 @@
+localRNG = function(seed=NULL, expr) {
+  if(is.null(seed)) {
+    force(expr)
+    return(invisible())
+  } else {
+    prevSeed = .Random.seed
+    set.seed(seed)
+    force(expr)
+    .Random.seed = prevSeed
+  }
+}
