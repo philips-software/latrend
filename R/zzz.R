ca4938ca4f1aed67e73a688eef1cfbe08b57a218
Niek Den Teuling <niek.den.teuling@philips.com>
2020-06-05 13:27:07 +0200

    Option for printing default opts

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,27 @@
 #' @import data.table
 #' @import assertthat
+#' @import methods
 #' @import magrittr
 #' @import foreach
 #' @import ggplot2
 
 .onLoad = function(libname, pkgname) {
   opts = list(
-    cluslong.verbose=R.utils::Verbose(threshold=-1),
-    cluslong.response='Value',
-    cluslong.time='Time',
-    cluslong.id='Id',
-    cluslong.clusterNames=LETTERS,
-    cluslong.printSharedModelArgs=FALSE)
+    cluslong.verbose = R.utils::Verbose(threshold=-1),
+    cluslong.response = 'Value',
+    cluslong.time = 'Time',
+    cluslong.id = 'Id',
+    cluslong.clusterNames = LETTERS,
+    cluslong.printOptions = FALSE,
+    cluslong.printSharedModelArgs = FALSE)
 
   optMask = !(names(opts) %in% names(options()))
   if(any(optMask)) {
     options(opts[optMask])
-    packageStartupMessage('Default options:')
-    packageStartupMessage(paste0('\t', names(opts[optMask]), ' = ', opts[optMask], collapse='\n'))
+
+    if(getOption('cluslong.printOptions', default=FALSE)) {
+      packageStartupMessage('Default options:')
+      packageStartupMessage(paste0('\t', names(opts[optMask]), ' = ', opts[optMask], collapse='\n'))
+    }
   }
 }
