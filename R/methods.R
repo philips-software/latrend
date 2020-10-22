63b24a7cbe91e178facf76997d9b50769a728e98
Niek Den Teuling <niek.den.teuling@philips.com>
2020-10-22 12:06:07 +0200

    lcMethods generates methods using Cartesian product

---
+++ R/methods.R
@@ -0,0 +1,73 @@
+#'  @include method.R
+
+#' @export
+#' @title Generate a list of lcMethod objects
+#' @description Generates a list of `lcMethod` objects for all combinations of the provided argument values.
+#' @param method The `lcMethod` to use as the template, which will be updated for each of the other arguments.
+#' @param ... Any other arguments to update the `lcMethod` definition with. Values must be `scalar`, `vector`, `list`, or encapsulated in a `.()` call.
+#' Arguments wrapped in `.()` are passed as-is to the model call, ensuring a readable method.
+#' Arguments comprising a single `symbol` (e.g. a variable name) are interpreted as a constant. To force evaluation, specify `arg=(var)` or `arg=force(var)`.
+#' Arguments of type `vector` or `list` are split across a series of method fit calls.
+#' Arguments of type `scalar` are constant across the method fits.
+#' If a `list` is intended to be passed as a constant argument, then specifying `arg=.(listObject)` results in it being treated as such.
+#' @param envir The `environment` in which to evaluate the method arguments.
+#' @return A `list` of `lcMethod` objects.
+#' @examples
+#' data(latrendData)
+#' baseMethod <- lcMethodKML("Y", id = "Id", time = "Time")
+#' methods <- lcMethods(baseMethod, nClusters = 1:6)
+#'
+#' nclus <- 1:6
+#' methods <- lcMethods(baseMethod, nClusters = nclus)
+#'
+#' methods <- lcMethods(baseMethod, nClusters = 3, center = .(mean, mean, median))
+#' length(methods) # 3
+#'
+#' methods <- lcMethods(baseMethod, nClusters = 1:3, center = .(mean, mean, median))
+#' length(methods) # 9
+lcMethods = function(method, ..., envir = NULL) {
+  assert_that(is.lcMethod(method))
+
+  envir = lcMethod.env(method, parent.frame(), envir)
+
+  mc = match.call()[-1]
+  assert_that(not('' %in% names(mc)), msg = 'method arguments must be named')
+  argNames = names(mc) %>% setdiff(c('method', 'envir'))
+  argCalls = mc[argNames]
+
+  nameMask = vapply(argCalls, is.name, FUN.VALUE = FALSE)
+  dotMask = vapply(argCalls, function(x) is.call(x) && x[[1]] == '.', FUN.VALUE = FALSE)
+  evalMask = !nameMask & !dotMask
+
+  nameArgs = lapply(which(nameMask), function(i) as.list(argCalls[[i]]))
+  dotArgs = lapply(which(dotMask), function(i) as.list(argCalls[[i]][-1]))
+  evalArgs = lapply(argCalls[evalMask], eval, envir = parent.frame())
+
+  assert_that(
+    all(vapply(nameArgs, is.list, FUN.VALUE = FALSE)),
+    all(vapply(dotArgs, is.list, FUN.VALUE = FALSE)),
+    all(vapply(evalArgs, is.vector, FUN.VALUE = FALSE)),
+    msg = 'The processed argument lists are in an unexpected format. Please report this issue.'
+  )
+
+  # combine the different arguments into a name-sorted list
+  allArgs = c(nameArgs, dotArgs, evalArgs)
+  allArgs = allArgs[sort(names(allArgs))]
+  argNs = lengths(allArgs)
+
+  # generate combinations
+  combIdx = lapply(argNs, seq_len) %>%
+    expand.grid() %>%
+    as.matrix()
+
+  # generate method list
+  if (nrow(combIdx) == 0) {
+    list(method)
+  }
+  else {
+    apply(combIdx, 1, function(idx) {
+      methodArgs = mapply('[[', allArgs, idx)
+      do.call(update, c(object = method, methodArgs, envir = envir))
+    })
+  }
+}
--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -1,47 +1,67 @@
 context('lcMethods')
 
+
+test_that('vector argument', {
+  methods = lcMethods(lcMethodTestKML(), nClusters=1:3)
+  expect_is(methods, 'list')
+  expect_length(methods, 3)
+})
+
 test_that('default', {
-  lcMethods(lcMethodTestKML()) %>%
-    expect_is('list') %>%
-    expect_length(1)
+  methods = lcMethods(lcMethodTestKML())
+  expect_is(methods, 'list')
+  expect_length(methods, 1)
 })
 
 test_that('var', {
   kml = lcMethodTestKML()
-  lcMethods(kml) %>%
-    expect_is('list') %>%
-    expect_length(1)
+  methods = lcMethods(kml)
+  expect_is(methods, 'list')
+  expect_length(methods, 1)
 })
 
 
 test_that('scalar argument', {
-  lcMethods(lcMethodTestKML(), nClusters=2) %>%
-    expect_is('list') %>%
-    expect_length(1) %T>%
-    {expect_equal(.[[1]]$nClusters, 2)}
+  methods = lcMethods(lcMethodTestKML(), nClusters=2)
+  expect_is(methods, 'list')
+  expect_length(methods, 1)
+  expect_equal(methods[[1]]$nClusters, 2)
 })
 
-test_that('vector argument', {
-  lcMethods(lcMethodTestKML(), testLongData, nClusters=1:3) %>%
-    expect_is('list') %>%
-    expect_length(3)
-})
 
 test_that('var with vector argument', {
   kml = lcMethodTestKML()
-  lcMethods(kml, testLongData, nClusters=1:3) %>%
-    expect_is('list') %>%
-    expect_length(3)
+  methods = lcMethods(kml, nClusters=1:3)
+  expect_is(methods, 'list')
+  expect_length(methods, 3)
 })
 
 test_that('.() argument', {
   a = 1
   b = 2
-  methods = lcMethods(lcMethodTestKML(), testLongData, nClusters=.(a,b)) %>%
-    expect_is('list') %>%
-    expect_length(2)
+  methods = lcMethods(lcMethodTestKML(), nClusters=.(a,b))
+  expect_is(methods, 'list')
+  expect_length(methods, 2)
 
   expect_equal(deparse(methods[[1]][['nClusters', eval=FALSE]]), 'a')
   expect_equal(deparse(methods[[2]][['nClusters', eval=FALSE]]), 'b')
 })
 
+test_that('cartesian', {
+  a = 1
+  b = 2
+  methods = lcMethods(lcMethodTestKML(), maxIt = 10, seed = .(a, b), nClusters=1:3)
+  expect_is(methods, 'list')
+  expect_length(methods, 1 * 2 * 3)
+
+  df = lapply(methods, as.data.table) %>% rbindlist()
+  expect_true(all(df$maxIt == 10))
+  expect_equal(df$seed, rep(c('a', 'b'), each = 3))
+  expect_equal(df$nClusters, rep(1:3, 2))
+})
+
+test_that('unnamed argument', {
+  expect_error({
+    lcMethods(lcMethodTestKML(), 2)
+  })
+})
