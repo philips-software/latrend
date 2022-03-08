8b3a4b581587ce469f0c62d503215189e0f7952e
Niek Den Teuling <niekdt@users.noreply.github.com>
2022-03-08 23:52:51 +0100

    Suppress mixAK warnings in tests

--- tests/testthat/test-mixak.R
+++ tests/testthat/test-mixak.R
@@ -7,17 +7,22 @@ tests = c(DEFAULT_LATREND_TESTS) %>% setdiff('cluster-three')
 make.mixak = function(id, time, response, ...) {
   form = as.formula(sprintf('%s ~ 1', response))
   rand = as.formula(sprintf('~ %s', time))
-  lcMethodMixAK_GLMM(fixed = form, time = time, id = id, random = rand, ..., seed = 1) %>% evaluate()
+  m = lcMethodMixAK_GLMM(fixed = form, time = time, id = id, random = rand, ..., seed = 1)
+  evaluate(m)
 }
 
 test_that('default', {
   expect_true({
-    test.latrend('lcMethodMixAK_GLMM', instantiator = make.mixak, tests = tests, args = list(PED = FALSE))
+    suppressWarnings({
+      test.latrend('lcMethodMixAK_GLMM', instantiator = make.mixak, tests = tests, args = list(PED = FALSE))
+    })
   })
 })
 
 test_that('multichain', {
   expect_true({
-    test.latrend('lcMethodMixAK_GLMM', instantiator = make.mixak, tests = tests, args = list(PED = TRUE))
+    suppressWarnings({
+      test.latrend('lcMethodMixAK_GLMM', instantiator = make.mixak, tests = tests, args = list(PED = TRUE))
+    })
   })
 })
