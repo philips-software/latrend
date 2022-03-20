378c77367c3a92947974ae1332785ecb3362955c
Niek Den Teuling <niekdt@users.noreply.github.com>
2022-03-21 00:51:00 +0100

    Resolved global variables warning

--- R/zzz.R
+++ R/zzz.R
@@ -164,13 +164,16 @@ NULL
   }
 }
 
-globalVariables(c('.', '.name', '.group', '.method', '.ROW_INDEX', '.Mean', '.Block',
-  'i', 'iseed', 'N', 'i.N', 'g', 'fun', 'method', 'plotTrajs', 'cl',
-  'Prob', 'Cluster', 'Class', 'Value', 'Id', 'Time',
-  'modelMethod', 'modelData', 'modelCall',
-  'weighted.mean',
-  'Mu', 'Mu.cluster', 'Mu.class', 'Mu.fixed', 'Mu.random',
-  'warning.Verbose',
-  'TVEMMixNormal',
-  '.Component'
-))
+globalVariables(
+  c(
+    '.', '..time', '.Fill', '.name', '.group', '.method', '.ROW_INDEX', '.Mean', '.Block',
+    'i', 'iseed', 'N', 'i.N', 'g', 'fun', 'method', 'plotTrajs', 'cl',
+    'Prob', 'Cluster', 'Class', 'Value', 'Id', 'Time',
+    'modelMethod', 'modelData', 'modelCall',
+    'weighted.mean',
+    'Mu', 'Mu.cluster', 'Mu.class', 'Mu.fixed', 'Mu.random',
+    'warning.Verbose',
+    'TVEMMixNormal',
+    '.Component'
+  )
+)
