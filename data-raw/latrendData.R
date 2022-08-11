f215ad838c130d840a6eb0e03ffa7c913e17a34a
Niek Den Teuling <niek.den.teuling@philips.com>
2022-08-11 15:17:55 +0200

    Modified group sizes and group trajectories of latrendData to make it easier to fit

--- data-raw/latrendData.R
+++ data-raw/latrendData.R
@@ -4,7 +4,7 @@ library(latrend)
 # Example data ####
 set.seed(1)
 latrendData = generateLongData(
-  sizes = c(90, 80, 30),
+  sizes = c(80, 70, 50),
   fixed = Y ~ 1,
   cluster = ~ Time + I(Time^2),
   random = ~ Time,
@@ -12,10 +12,11 @@ latrendData = generateLongData(
   data = data.frame(Time = seq(0, 2, length.out = 10)),
   fixedCoefs = 0,
   clusterCoefs = cbind(c(-1, 1.5, -.4), c(0, 0, 0), c(.25, .25, -.4)),
-  randomScales = cbind(c(.3, .05), c(.2, .1), c(.1, .1)),
-  noiseScales = c(.1, .1, .1),
+  randomScales = cbind(c(.3, .05), c(.2, .02), c(.1, .1)),
+  noiseScales = c(.1, .05, .05),
   clusterNames = paste('Class', 1:3),
-  shuffle = FALSE) %>%
+  shuffle = FALSE
+) %>%
   .[, .(Id, Time, Y, Class)] %>%
   as.data.frame()
 
